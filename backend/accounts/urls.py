b3798c2805277836ad3bd3c497a75e0cdefe755b
jayo14 <ayobamidayo2007@gmail.com>
2026-07-14 21:25:40 +0100

    wire reset views import

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterView, MeView
+from .views import RegisterView, MeView, PasswordResetRequestView, PasswordResetConfirmView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
