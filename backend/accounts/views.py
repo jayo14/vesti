1e63b07b603d72dcf1f1464cf7e76dfe3a69f18f
jayo14 <ayobamidayo2007@gmail.com>
2026-07-14 21:34:50 +0100

    add reset view imports

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,6 +1,16 @@
-from rest_framework import generics, permissions
+from django.conf import settings
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import send_mail
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from rest_framework import generics, permissions, status
 from rest_framework.response import Response
-from .serializers import RegisterSerializer, UserSerializer
+from .serializers import (
+    RegisterSerializer,
+    UserSerializer,
+    PasswordResetRequestSerializer,
+    PasswordResetConfirmSerializer,
+)
 from .models import User
 
 class RegisterView(generics.CreateAPIView):
