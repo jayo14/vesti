29b115eafb5d2839f322a4c80f2694a2d7b8f295
jayo14 <ayobamidayo2007@gmail.com>
2026-07-15 07:43:55 +0100

    materials list

--- frontend/src/lib/materials.ts
+++ frontend/src/lib/materials.ts
@@ -1,1 +1,11 @@
-export default {};
+export const materials = [
+  "Cotton", "Silk", "Denim", "Leather", "Linen", "Wool",
+  "Polyester", "Rayon", "Velvet", "Cashmere",
+] as const;
+
+export const colours = [
+  "Black", "White", "Red", "Blue", "Green", "Beige",
+  "Navy", "Grey", "Pink", "Purple",
+] as const;
+
+export const fits = ["Slim", "Regular", "Oversized"] as const;
