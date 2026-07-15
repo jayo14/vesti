abd5fb8af5e4c98797eddb848c00474fa79a68bc
jayo14 <ayobamidayo2007@gmail.com>
2026-07-15 07:44:04 +0100

    product data

--- frontend/src/lib/products.ts
+++ frontend/src/lib/products.ts
@@ -1,1 +1,12 @@
-export default {};
+export const products = [
+  { id: 1, name: "Urban Jacket", price: 89, rating: 4, tag: "New" },
+  { id: 2, name: "Linen Shirt", price: 49, rating: 5, tag: "Sale" },
+  { id: 3, name: "Slim Trousers", price: 65, rating: 3, tag: "Popular" },
+  { id: 4, name: "Canvas Sneakers", price: 72, rating: 4, tag: "New" },
+  { id: 5, name: "Wool Scarf", price: 38, rating: 5, tag: "Sale" },
+  { id: 6, name: "Leather Belt", price: 44, rating: 4 },
+];
+
+export function getProduct(id: number) {
+  return products.find((p) => p.id === id);
+}
